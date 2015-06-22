97fd71dc80eb87c8e275612958d3ca415a658541
Oscar <newman.oscar@gmail.com>
2015-06-22 15:37:39 -0500

    pods and gitignore

---
+++ schedules/Pods/Target Support Files/Pods-schedules-CSStickyHeaderFlowLayout/Pods-schedules-CSStickyHeaderFlowLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_schedules_CSStickyHeaderFlowLayout : NSObject
+@end
+@implementation PodsDummy_Pods_schedules_CSStickyHeaderFlowLayout
+@end
---
+++ schedules/Pods/Target Support Files/Pods-schedules/Pods-schedules-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_schedules : NSObject
+@end
+@implementation PodsDummy_Pods_schedules
+@end
